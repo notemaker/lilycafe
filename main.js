c4d361ff63d1ba77c13a2f6af3cd3b6e3de0f130
notemaker <61339711+notemaker@users.noreply.github.com>
2025-02-28 23:40:21 +0800

    增加人數
    表格及gas中增加人數欄位

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-var script_url = "https://script.google.com/macros/s/AKfycby-bWjnrG0PRdCoYNV_bwHEL52kqxxUviMPUiKMM3KoUBLB4F3hU7xsJjzvWbrxTiM-hA/exec";
+var script_url = "https://script.google.com/macros/s/AKfycbwaROA9OlZ1oz4XL0wRZTfcHd_aYYrRmyS4BaY0DpN8Dbh3V8ootYgg4R7AmO6LL0qlCw/exec";
 $(document).ready(function ($) {
     $("#google-sheet").submit(function (event) {
-        $('#date, #time, #name, #phone').prop("disabled", false);
+        $('#date, #time, #man, #name, #phone').prop("disabled", false);
         var title= $("#restitle").val();
         var date = $("#date").val();
         var time = $("#time").val();
+        var man=$("#man").val();
         var name = $("#name").val();
         var phone = $("#phone").val();
-        var url = script_url + "?callback=ctrlq&date=" + date + "&time=" + time + "&name=" + name + "&phone=" + phone + "&action=create";
+        var url = script_url + "?callback=ctrlq&date=" + date + "&time=" + time + "&man=" + man + "&name=" + name + "&phone=" + phone + "&action=create";
         var request = $.ajax({
             url: url,
             type: "POST"
@@ -16,7 +17,7 @@ $(document).ready(function ($) {
             window.location.reload();
         });
         request.always(function () {
-           $('#date, #time, #name, #phone').prop("disabled", true);
+           $('#date, #time, #man, #name, #phone').prop("disabled", true);
         });
         event.preventDefault();
 
